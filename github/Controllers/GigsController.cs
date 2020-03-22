d030d20cf9534fac9e04da3b437ffdb2c642b7d1
UxDeveloper82 <electronicsmf@aol.com>
2020-03-22 18:35:03 -0400

    git init

---
+++ github/Controllers/GigsController.cs
@@ -0,0 +1,58 @@
+﻿using github.Models;
+using github.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace github.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewmodel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+
+            };
+            
+            return View(viewmodel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == User.Identity.GetUserId());
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                ArtistId = artistId,
+                DateTime = viewModel.DateTime,
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+    }
+}
